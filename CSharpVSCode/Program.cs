87da10ad49695686cb6936327646c36b85d08dfa
aashayg <51148558+aashayg@users.noreply.github.com>
2022-04-06 12:10:56 +0530

    Sample Security Reference Project

---
+++ CSharpVSCode/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string Username = "Winjit";
+string Password = "Winjit@123";
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Username);
+Console.WriteLine(Password);
